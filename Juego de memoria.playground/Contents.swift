a89a8ccbcd6eaf3b448d86a212cf0299da90f29e
Sergio García <sergiogm.amurrio@gmail.com>
2016-02-04 16:27:26 +0100

    Changes

--- Juego de memoria.playground/Contents.swift
+++ Juego de memoria.playground/Contents.swift
@@ -5,19 +5,19 @@
 
 import UIKit
 
-
-for var i = 0; i <= 100; i++ {
-    if i % 5 == 0 {
-        print("# \(i) Bingo!!!")
+let numbers = 0...100
+for number in numbers {
+    if number % 5 == 0 {
+        print("# \(number) Bingo!!!")
     }
-    if i % 2 == 0 {
-        print("# \(i) par!!!")
+    if number % 2 == 0 {
+        print("# \(number) par!!!")
     }
-    if i % 2 != 0 {
-        print("# \(i) impar!!!")
+    if number % 2 != 0 {
+        print("# \(number) impar!!!")
     }
-    if i > 30 &&
-        i < 40 {
-        print("# \(i) Viva Swift!!!")
+    if number > 30 &&
+        number < 40 {
+        print("# \(number) Viva Swift!!!")
     }
 }
